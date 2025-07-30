981e9eb21b868fa1a824cec3fb9c763489b0fe2d
paco9720 <62359183+Paco9720@users.noreply.github.com>
2025-07-29 22:28:29 -0600

    Add files via upload

--- nvim/lua/plugs.lua
+++ nvim/lua/plugs.lua
@@ -12,22 +12,74 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Lista de plugins
-require("lazy").setup({
-  -- Sustituto de auto-pairs
-  { "jiangmiao/auto-pairs" }, -- sigue funcionando aunque es Vimscript
+return require("lazy").setup({
+  -- --- Gestores de Mason (DEBEN CARGARSE PRIMERO) ---
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup()
+    end,
+  },
 
-  -- Dependencia general para otros plugins
+  -- --- Dependencia de UI general (nui.nvim) ---
+  -- Se declara aquí como un plugin de nivel superior para asegurar que se cargue
+  -- antes que otros plugins que puedan depender de él para componentes de UI.
+  {
+    "MunifTanjim/nui.nvim",
+    -- No se especifica 'lazy = true' o un evento, forzando su carga temprana.
+    -- Esto es crucial para plugins como avante.nvim que lo requieren en su config.
+  },
+
+  -- --- Plugin avante.nvim ---
+  {
+    "yetone/avante.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      -- 'MunifTanjim/nui.nvim' ya está declarado arriba, pero se puede mantener aquí
+      -- como recordatorio de la dependencia si se desea. Lazy.nvim no lo instalará dos veces.
+    },
+    config = function()
+      require("avante").setup({
+        -- Aquí puedes añadir opciones de configuración para Avante
+        -- Por ejemplo:
+        -- debug = true, -- Descomenta para depurar
+        -- handlers = require("avante.config.handlers").build({
+        --   tsserver = true, -- Habilita tsserver
+        --   lua_ls = {
+        --     -- Configuración específica para lua_ls
+        --     Lua = {
+        --       workspace = { checkThirdParty = false },
+        --       telemetry = { enable = false },
+        --     },
+        --   },
+        -- }),
+      })
+
+      -- Opcional: Si quieres mantener nvim-lspconfig para servidores no gestionados por Avante,
+      -- puedes cargarlo después y configurar algunos.
+      -- require("lspconfig").rust_analyzer.setup({})
+    end,
+  },
+
+  -- --- Otros plugins ---
+  { "jiangmiao/auto-pairs" },
   { "nvim-lua/plenary.nvim" },
-
-  -- Telescope con versión específica
   {
     "nvim-telescope/telescope.nvim",
     version = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
-
-  -- GitHub Copilot
   { "github/copilot.vim" },
-})
 
+  -- Si otros plugins también usan nui.nvim, ya está declarado arriba.
+})
 
